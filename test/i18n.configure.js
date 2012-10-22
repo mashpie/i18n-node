0bd1bbb8ff11f7202ba858e6163bd16bdf50dfb6
Jevgeni Kiski <jevgeni.kiski@skype.net>
2012-10-22 15:33:20 +0300

    Indent 4 -> 2

--- test/i18n.configure.js
+++ test/i18n.configure.js
@@ -1,18 +1,18 @@
 // Run $ expresso
 var i18n = require('../i18n'),
-    assert = require('assert'),
-    fs = require('fs');
+  assert = require('assert'),
+  fs = require('fs');
 
 i18n.configure({
-  locales: ['en_US', 'de_DE'],
-  register: global,
-  directory: './testlocales',
-  extension: '.json',
-  debug: false
+  locales:['en_US', 'de_DE'],
+  register:global,
+  directory:'./testlocales',
+  extension:'.json',
+  debug:false
 });
 
 module.exports = {
-  'check set/getLocale': function () {
+  'check set/getLocale':function () {
     var loc = i18n.getLocale();
     assert.equal('en', loc.language, 'should return default setting');
     assert.equal('US', loc.region, 'should return default setting');
@@ -21,7 +21,7 @@ module.exports = {
     assert.equal('de', i18n.getLocale().language, 'should return the new setting');
   },
 
-  'check singular': function () {
+  'check singular':function () {
     i18n.setLocale('en_US');
     assert.equal(__('Hello'), 'Hello');
     assert.equal(__('Hello %s, how are you today?', 'Marcus'), 'Hello Marcus, how are you today?');
--- test/i18n.test.js
+++ test/i18n.test.js
@@ -1,28 +1,28 @@
 // Run $ expresso
 var i18n = require('../i18n'),
-    assert = require('assert');
+  assert = require('assert');
 
 i18n.configure({
   // setup some locales - other locales default to en silently
-  locales: ['en_US', 'de_DE'],
+  locales:['en_US', 'de_DE'],
 
   // where to register __() and __n() to, might be "global" if you know what you are doing
-  register: global
+  register:global
 
 });
 
 module.exports = {
-  'check version': function () {
+  'check version':function () {
     assert.equal(i18n.version, '0.3.5');
   },
 
-  'check set/getLocale language only': function () {
+  'check set/getLocale language only':function () {
     assert.equal('en', i18n.getLocale().language, 'should return default setting');
     assert.equal('de', i18n.setLocale('de').language, 'should return the new setting');
     assert.equal('de', i18n.setLocale('ec').language, 'should keep previous setting');
   },
 
-  'check set/getLocale': function () {
+  'check set/getLocale':function () {
     var req = {};
     i18n.setLocale('en');
     assert.equal('en', i18n.getLocale().language, 'should return default setting');
@@ -33,7 +33,7 @@ module.exports = {
     assert.equal('de', i18n.getLocale().language, 'should return the previous default setting');
   },
 
-  'check singular': function () {
+  'check singular':function () {
     i18n.setLocale('en_US');
     assert.equal(__('Hello'), 'Hello');
     assert.equal(__('Hello %s, how are you today?', 'Marcus'), 'Hello Marcus, how are you today?');
@@ -46,15 +46,15 @@ module.exports = {
 
   },
 
-  'check singular placeholders': function () {
-      i18n.setLocale('en_US');
-      assert.equal(__('Click {LINK_START}here{LINK_END} and have some free beer', {'LINK_START': '<a href="mylink">', 'LINK_END': '</a>'}), 'Click <a href="mylink">here</a> and have some free beer');
+  'check singular placeholders':function () {
+    i18n.setLocale('en_US');
+    assert.equal(__('Click {LINK_START}here{LINK_END} and have some free beer', {'LINK_START':'<a href="mylink">', 'LINK_END':'</a>'}), 'Click <a href="mylink">here</a> and have some free beer');
 
-      i18n.setLocale('de_DE');
-      assert.equal(__('Click {LINK_START}here{LINK_END} and have some free beer', {'LINK_START': '<a href="mylink">', 'LINK_END': '</a>'}), 'Klicken Sie <a href="mylink">hier</a> und haben einige Freibier');
+    i18n.setLocale('de_DE');
+    assert.equal(__('Click {LINK_START}here{LINK_END} and have some free beer', {'LINK_START':'<a href="mylink">', 'LINK_END':'</a>'}), 'Klicken Sie <a href="mylink">hier</a> und haben einige Freibier');
   },
 
-  'check plural': function () {
+  'check plural':function () {
     i18n.setLocale('en_US');
     var singular = __n('%s cat', '%s cats', 1);
     var plural = __n('%s cat', '%s cats', 3);
@@ -68,32 +68,34 @@ module.exports = {
     assert.equal(plural, '3 Katzen');
   },
 
-  'check nested plural': function () {
-        i18n.setLocale('en_US');
-        var singular = __n('There is one monkey in the %%s', 'There are %d monkeys in the %%s', 1, __('tree'));
-        var plural = __n('There is one monkey in the %%s', 'There are %d monkeys in the %%s', 3, __('tree'));
-        assert.equal(singular, 'There is one monkey in the tree');
-        assert.equal(plural, 'There are 3 monkeys in the tree');
+  'check nested plural':function () {
+    i18n.setLocale('en_US');
+    var singular = __n('There is one monkey in the %%s', 'There are %d monkeys in the %%s', 1, __('tree'));
+    var plural = __n('There is one monkey in the %%s', 'There are %d monkeys in the %%s', 3, __('tree'));
+    assert.equal(singular, 'There is one monkey in the tree');
+    assert.equal(plural, 'There are 3 monkeys in the tree');
 
-        i18n.setLocale('de_DE');
-        singular = __n('There is one monkey in the %%s', 'There are %d monkeys in the %%s', 1, __('tree'));
-        plural = __n('There is one monkey in the %%s', 'There are %d monkeys in the %%s', 3, __('tree'));
-        assert.equal(singular, 'Im Baum sitzt ein Affe');
-        assert.equal(plural, 'Im Baum sitzen 3 Affen');
-    },
+    i18n.setLocale('de_DE');
+    singular = __n('There is one monkey in the %%s', 'There are %d monkeys in the %%s', 1, __('tree'));
+    plural = __n('There is one monkey in the %%s', 'There are %d monkeys in the %%s', 3, __('tree'));
+    assert.equal(singular, 'Im Baum sitzt ein Affe');
+    assert.equal(plural, 'Im Baum sitzen 3 Affen');
+  },
 
-  'check variables': function () {
+  'check variables':function () {
     var i = 0;
     i18n.setLocale('en_US');
     var greetings = ['Hi', 'Hello', 'Howdy'];
     for (i = 0; i < greetings.length; i++) {
       assert.equal(greetings[i], __(greetings[i]));
-    };
+    }
+    ;
 
     i18n.setLocale('de_DE');
     var greetingsDE = ['Hi', 'Hallo', 'Hallöchen'];
     for (i = 0; i < greetings.length; i++) {
       assert.equal(greetingsDE[i], __(greetings[i]));
-    };
+    }
+    ;
   }
 };
