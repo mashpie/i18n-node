05a5eeeada193df37acac6f0e9c832c2db3603ef
Marcus Spiegel <spiegel@uscreen.de>
2013-01-27 22:31:51 +0100

    little changes according to jslint, added jslint directive

--- i18n.js
+++ i18n.js
@@ -1,7 +1,8 @@
+/*jslint nomen: true, undef: true, sloppy: true, white: true, stupid: true, passfail: false, node: true, indent: 2 */
 /**
  * @author      Created by Marcus Spiegel <marcus.spiegel@gmail.com> on 2011-03-25.
  * @link        https://github.com/mashpie/i18n-node
- * @license		  http://creativecommons.org/licenses/by-sa/3.0/
+ * @license     http://creativecommons.org/licenses/by-sa/3.0/
  *
  * @version     0.3.5
  */
@@ -47,7 +48,7 @@ i18n.configure = function (opt) {
 
   // write new locale information to disk
   if (typeof opt.updateFiles === 'boolean') {
-    updateFiles = opt.updateFiles
+    updateFiles = opt.updateFiles;
   }
 
   // where to store json files
@@ -77,27 +78,24 @@ i18n.init = function (request, response, next) {
   }
 };
 
-i18n.__ = function () {
-  var locale;
+i18n.__ = function (phrase) {
+  var locale, msg;
   if (this && this.scope) {
     locale = this.scope.locale;
   }
-  var msg = translate(locale, arguments[0]);
+  msg = translate(locale, phrase);
   if (arguments.length > 1) {
     msg = vsprintf(msg, Array.prototype.slice.call(arguments, 1));
   }
   return msg;
 };
 
-i18n.__n = function () {
-  var locale;
+i18n.__n = function (singular, plural, count) {
+  var locale, msg;
   if (this && this.scope) {
     locale = this.scope.locale;
   }
-  var singular = arguments[0];
-  var plural = arguments[1];
-  var count = arguments[2];
-  var msg = translate(locale, singular, plural);
+  msg = translate(locale, singular, plural);
 
   if (parseInt(count, 10) > 1) {
     msg = vsprintf(msg.other, [count]);
@@ -112,12 +110,12 @@ i18n.__n = function () {
   return msg;
 };
 
-// either gets called like 
+// either gets called like
 // setLocale('en') or like
 // setLocale(req, 'en')
 i18n.setLocale = function (arg1, arg2) {
-  var request = undefined,
-      target_locale = arg1;
+  var target_locale = arg1,
+      request;
 
   if (arg2 && locales[arg2]) {
     request = arg1;
@@ -143,12 +141,14 @@ i18n.getLocale = function (request) {
 };
 
 i18n.overrideLocaleFromQuery = function (req) {
-  if (req == null) {
+  if (req === null) {
     return;
   }
   var urlObj = url.parse(req.url, true);
   if (urlObj.query.locale) {
-    if (debug) console.log("Overriding locale from query: " + urlObj.query.locale);
+    if (debug) {
+      console.log("Overriding locale from query: " + urlObj.query.locale);
+    }
     i18n.setLocale(req, urlObj.query.locale.toLowerCase());
   }
 };
@@ -156,6 +156,7 @@ i18n.overrideLocaleFromQuery = function (req) {
 // ===================
 // = private methods =
 // ===================
+
 // guess language setting based on http headers
 
 function guessLanguage(request) {
@@ -171,8 +172,8 @@ function guessLanguage(request) {
 
     if (language_header) {
       language_header.split(',').forEach(function (l) {
-        var header = l.split(';', 1)[0];
-        var lr = header.split('-', 2);
+        var header = l.split(';', 1)[0],
+            lr = header.split('-', 2);
         if (lr[0]) {
           languages.push(lr[0].toLowerCase());
         }
@@ -205,7 +206,9 @@ function guessLanguage(request) {
 
 function translate(locale, singular, plural) {
   if (locale === undefined) {
-    if (debug) console.warn("WARN: No locale found - check the context of the call to $__. Using " + defaultLocale + " (set by request) as current locale");
+    if (debug) {
+      console.warn("WARN: No locale found - check the context of the call to $__. Using " + defaultLocale + " (set by request) as current locale");
+    }
     locale = defaultLocale;
   }
 
@@ -233,22 +236,26 @@ function translate(locale, singular, plural) {
 // try reading a file
 
 function read(locale) {
-  var localeFile = {};
-  var file = locate(locale);
+  var localeFile = {},
+      file = locate(locale);
   try {
-    if (verbose) console.log('read ' + file + ' for locale: ' + locale);
+    if (verbose) {
+      console.log('read ' + file + ' for locale: ' + locale);
+    }
     localeFile = fs.readFileSync(file);
     try {
       // parsing filecontents to locales[locale]
       locales[locale] = JSON.parse(localeFile);
-    } catch (e) {
+    } catch (parseError) {
       console.error('unable to parse locales from file (maybe ' + file + ' is empty or invalid json?): ', e);
     }
-  } catch (e) {
+  } catch (readError) {
     // unable to read, so intialize that file
     // locales[locale] are already set in memory, so no extra read required
     // or locales[locale] are empty, which initializes an empty locale.json file
-    if (verbose) console.log('initializing ' + file);
+    if (verbose) {
+      console.log('initializing ' + file);
+    }
     write(locale);
   }
 }
@@ -256,17 +263,21 @@ function read(locale) {
 // try writing a file in a created directory
 
 function write(locale) {
+  var stats, target, tmp;
+
   // don't write new locale information to disk if updateFiles isn't true
-  if(!updateFiles) {
+  if (!updateFiles) {
     return;
   }
 
-  // creating directory if necessary 
+  // creating directory if necessary
   try {
-    var stats = fs.lstatSync(directory);
+    stats = fs.lstatSync(directory);
   } catch (e) {
-    if (debug) console.log('creating locales dir in: ' + directory);
-    fs.mkdirSync(directory, 0755);
+    if (debug) {
+      console.log('creating locales dir in: ' + directory);
+    }
+    fs.mkdirSync(directory, parseInt('755', 8));
   }
 
   // first time init has an empty file
@@ -276,12 +287,11 @@ function write(locale) {
 
   // writing to tmp and rename on success
   try {
-    var target = locate(locale),
-        tmp = target + ".tmp";
-
+    target = locate(locale);
+    tmp = target + ".tmp";
     fs.writeFileSync(tmp, JSON.stringify(locales[locale], null, "\t"), "utf8");
-    var Stats = fs.statSync(tmp);
-    if (Stats.isFile()) {
+    stats = fs.statSync(tmp);
+    if (stats.isFile()) {
       fs.renameSync(tmp, target);
     } else {
       console.error('unable to write locales to file (either ' + tmp + ' or ' + target + ' are not writeable?): ', e);
