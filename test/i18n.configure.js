cb79a25c8ebf055ff9c1102c30ad4a75cd1a553a
Marcus Spiegel <spiegel@uscreen.de>
2013-01-28 02:14:33 +0100

    ported tests to mocha

--- test/i18n.configure.js
+++ test/i18n.configure.js
@@ -1,7 +1,7 @@
 /*jslint nomen: true, undef: true, sloppy: true, white: true, stupid: true, passfail: false, node: true, plusplus: true, indent: 2 */
 
-var i18n = require('../i18n'),
-    assert = require('assert'),
+var should = require("should"),
+    i18n = require('../i18n'),
     fs = require('fs');
 
 i18n.configure({
@@ -9,21 +9,36 @@ i18n.configure({
   register: global,
   directory: './testlocales',
   extension: '.json',
-  debug: false
+  debug: false,
+  verbose: false
 });
 
-module.exports = {
-  'check set/getLocale': function () {
-    var loc = i18n.getLocale();
-    assert.equal('en', i18n.getLocale(), 'should return default setting');
-    assert.equal('de', i18n.setLocale('de'), 'should return the new setting');
-    assert.equal('de', i18n.getLocale(), 'should return the new setting');
-  },
+describe('Configuration settings:', function () {
 
-  'check singular': function () {
-    i18n.setLocale('en');
-    assert.equal(__('Hello'), 'Hello');
-    assert.equal(__('Hello %s, how are you today?', 'Marcus'), 'Hello Marcus, how are you today?');
-    assert.equal(__('Hello %s, how are you today? How was your %s.', 'Marcus', __('weekend')), 'Hello Marcus, how are you today? How was your weekend.');
-  }
-};
+  after(function () {
+    var stats = fs.lstatSync('./testlocales');
+    should.exist(stats);
+    if (stats) {
+      fs.unlinkSync('./testlocales/de.json');
+      fs.unlinkSync('./testlocales/en.json');
+      fs.rmdirSync('./testlocales');
+    }
+  });
+
+  describe('when using a custom directory with a custom extension', function () {
+
+    it('the setLocale and getLocale should still be able to set current locale', function () {
+      should.equal('en', i18n.getLocale(), 'should return default setting');
+      should.equal('de', i18n.setLocale('de'), 'should return the new setting');
+      should.equal('de', i18n.getLocale(), 'should return the new setting');
+    });
+
+    it('and some basic translations should still work', function () {
+      i18n.setLocale('en');
+      should.equal(__('Hello'), 'Hello');
+      should.equal(__('Hello %s, how are you today?', 'Marcus'), 'Hello Marcus, how are you today?');
+      should.equal(__('Hello %s, how are you today? How was your %s.', 'Marcus', __('weekend')), 'Hello Marcus, how are you today? How was your weekend.');
+    });
+
+  });
+});
--- test/i18n.test.js
+++ test/i18n.test.js
@@ -1,81 +1,136 @@
 /*jslint nomen: true, undef: true, sloppy: true, white: true, stupid: true, passfail: false, node: true, plusplus: true, indent: 2 */
 
 var i18n = require('../i18n'),
-    assert = require('assert');
+    should = require("should");
 
 i18n.configure({
   locales: ['en', 'de'],
   register: global
 });
 
-module.exports = {
-  'check version': function () {
-    assert.equal(i18n.version, '0.3.6');
-  },
 
-  'check set/getLocale': function () {
-    assert.equal('en', i18n.getLocale(), 'should return default setting');
-    assert.equal('de', i18n.setLocale('de'), 'should return the new setting');
-    assert.equal('de', i18n.getLocale(), 'should return the new setting');
-    assert.equal('en', i18n.setLocale({}, 'en'), 'should return the request setting');
-    assert.equal('de', i18n.getLocale(), 'should return the previous default setting');
-  },
+describe('module setup', function () {
+  it('the version should be set', function () {
+    should.equal(i18n.version, '0.3.6');
+  });
+});
 
-  'check singular': function () {
-    i18n.setLocale('en');
-    assert.equal(__('Hello'), 'Hello');
-    assert.equal(__('Hello %s, how are you today?', 'Marcus'), 'Hello Marcus, how are you today?');
-    assert.equal(__('Hello %s, how are you today? How was your %s.', 'Marcus', __('weekend')), 'Hello Marcus, how are you today? How was your weekend.');
+describe('module api', function () {
 
-    i18n.setLocale('de');
-    assert.equal(__('Hello'), 'Hallo');
-    assert.equal(__('Hello %s, how are you today?', 'Marcus'), 'Hallo Marcus, wie geht es dir heute?');
-    assert.equal(__('Hello %s, how are you today? How was your %s.', 'Marcus', __('weekend')), 'Hallo Marcus, wie geht es dir heute? Wie war dein Wochenende.');
+  var req = {
+    "request": "GET /test",
+    __: i18n.__
+  };
 
-  },
+  describe('setLocal and getLocale', function () {
+    it('getLocale should return default setting', function () {
+      i18n.getLocale().should.equal('en');
+    });
 
-  'check plural': function () {
-    i18n.setLocale('en');
-    var singular = __n('%s cat', '%s cats', 1),
-        plural = __n('%s cat', '%s cats', 3);
-    assert.equal(singular, '1 cat');
-    assert.equal(plural, '3 cats');
+    it('setLocale should return the new setting', function () {
+      i18n.setLocale('de').should.equal('de');
+    });
 
-    i18n.setLocale('de');
-    singular = __n('%s cat', '%s cats', 1);
-    plural = __n('%s cat', '%s cats', 3);
-    assert.equal(singular, '1 Katze');
-    assert.equal(plural, '3 Katzen');
-  },
+    it('and getLocale should return the new setting', function () {
+      i18n.getLocale().should.equal('de');
+    });
 
-  'check nested plural': function () {
-    i18n.setLocale('en');
-    var singular = __n('There is one monkey in the %%s', 'There are %d monkeys in the %%s', 1, __('tree')),
-        plural = __n('There is one monkey in the %%s', 'There are %d monkeys in the %%s', 3, __('tree'));
-    assert.equal(singular, 'There is one monkey in the tree');
-    assert.equal(plural, 'There are 3 monkeys in the tree');
+    it('should return the current local setting, when used with 2 args', function () {
+      i18n.setLocale(req, 'en').should.equal('en');
+    });
 
-    i18n.setLocale('de');
-    singular = __n('There is one monkey in the %%s', 'There are %d monkeys in the %%s', 1, __('tree'));
-    plural = __n('There is one monkey in the %%s', 'There are %d monkeys in the %%s', 3, __('tree'));
-    assert.equal(singular, 'Im Baum sitzt ein Affe');
-    assert.equal(plural, 'Im Baum sitzen 3 Affen');
+    it('while getLocale should still return the previous global setting', function () {
+      i18n.getLocale().should.equal('de');
+    });
 
-  },
+    it('now getLocale should return local locale when used with local object as 1st arg', function () {
+      i18n.getLocale(req).should.equal('en');
+    });
+  });
 
-  'check variables': function () {
-    var i = 0,
-        greetings = ['Hi', 'Hello', 'Howdy'],
-        greetingsDE = ['Hi', 'Hallo', 'Hallöchen'];
+  describe('translating singulars', function () {
+    it('should return en translations as expected', function () {
+      i18n.setLocale('en');
+      should.equal(__('Hello'), 'Hello');
+      should.equal(__('Hello %s, how are you today?', 'Marcus'), 'Hello Marcus, how are you today?');
+      should.equal(__('Hello %s, how are you today? How was your %s.', 'Marcus', __('weekend')), 'Hello Marcus, how are you today? How was your weekend.');
+    });
 
-    i18n.setLocale('en');
-    for (i = 0; i < greetings.length; i++) {
-      assert.equal(greetings[i], __(greetings[i]));
-    }
+    it('and should return de translations as expected', function () {
+      i18n.setLocale('de');
+      should.equal(__('Hello'), 'Hallo');
+      should.equal(__('Hello %s, how are you today?', 'Marcus'), 'Hallo Marcus, wie geht es dir heute?');
+      should.equal(__('Hello %s, how are you today? How was your %s.', 'Marcus', __('weekend')), 'Hallo Marcus, wie geht es dir heute? Wie war dein Wochenende.');
+    });
+  });
 
-    i18n.setLocale('de');
-    for (i = 0; i < greetings.length; i++) {
-      assert.equal(greetingsDE[i], __(greetings[i]));
-    }
-  }
-};
+  describe('translating in scopes (ie.: request)', function(){
+    it('has to use local translation in en', function(){
+      i18n.setLocale(req, 'en').should.equal('en');
+      req.__('Hello').should.equal('Hello');
+    });
+
+    it('while the global translation remains untouched', function(){
+      should.equal(__('Hello'), 'Hallo');
+    });
+
+    it('and has to use local translation in de', function(){
+      i18n.setLocale(req, 'de').should.equal('de');
+      req.__('Hello').should.equal('Hallo');
+    });
+
+    it('still the global translation remains untouched', function(){
+      should.equal(__('Hello'), 'Hallo');
+    });
+  });
+
+  describe('translating basic plurals', function(){
+    it('should work as expected (to lazy to change from expresso)', function(){
+      i18n.setLocale('en');
+      var singular = __n('%s cat', '%s cats', 1),
+          plural = __n('%s cat', '%s cats', 3);
+      should.equal(singular, '1 cat');
+      should.equal(plural, '3 cats');
+
+      i18n.setLocale('de');
+      singular = __n('%s cat', '%s cats', 1);
+      plural = __n('%s cat', '%s cats', 3);
+      should.equal(singular, '1 Katze');
+      should.equal(plural, '3 Katzen');
+    });
+  });
+
+  describe('translating nested plurals', function(){
+    it('should work as expected (to lazy to change from expresso)', function(){
+      i18n.setLocale('en');
+      var singular = __n('There is one monkey in the %%s', 'There are %d monkeys in the %%s', 1, __('tree')),
+          plural = __n('There is one monkey in the %%s', 'There are %d monkeys in the %%s', 3, __('tree'));
+      should.equal(singular, 'There is one monkey in the tree');
+      should.equal(plural, 'There are 3 monkeys in the tree');
+
+      i18n.setLocale('de');
+      singular = __n('There is one monkey in the %%s', 'There are %d monkeys in the %%s', 1, __('tree'));
+      plural = __n('There is one monkey in the %%s', 'There are %d monkeys in the %%s', 3, __('tree'));
+      should.equal(singular, 'Im Baum sitzt ein Affe');
+      should.equal(plural, 'Im Baum sitzen 3 Affen');
+    });
+  });
+
+  describe('translating variables values', function(){
+    it('should work as expected (to lazy to change from expresso)', function(){
+      var i = 0,
+          greetings = ['Hi', 'Hello', 'Howdy'],
+          greetingsDE = ['Hi', 'Hallo', 'Hallöchen'];
+
+      i18n.setLocale('en');
+      for (i = 0; i < greetings.length; i++) {
+        should.equal(greetings[i], __(greetings[i]));
+      }
+
+      i18n.setLocale('de');
+      for (i = 0; i < greetings.length; i++) {
+        should.equal(greetingsDE[i], __(greetings[i]));
+      }
+    });
+  });
+});
