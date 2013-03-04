b7bb6dc254168185fe3eefc959dcf20a8cbd9689
Marcus Spiegel <spiegel@uscreen.de>
2013-03-04 14:22:47 +0100

    partial rewrite of config tests

--- test/i18n.configure.js
+++ test/i18n.configure.js
@@ -4,18 +4,22 @@ var should = require("should"),
     i18n = require('../i18n'),
     fs = require('fs');
 
-i18n.configure({
-  locales: ['en', 'de'],
-  register: global,
-  directory: './testlocales',
-  extension: '.json',
-  debug: false,
-  verbose: false
-});
-
 describe('Configuration settings:', function () {
 
-  after(function () {
+  var testScope = {};
+
+  beforeEach(function () {
+    i18n.configure({
+      locales: ['en', 'de'],
+      register: testScope,
+      directory: './testlocales',
+      extension: '.json',
+      debug: false,
+      verbose: false
+    });
+  });
+
+  afterEach(function () {
     var stats = fs.lstatSync('./testlocales');
     should.exist(stats);
     if (stats) {
@@ -27,18 +31,36 @@ describe('Configuration settings:', function () {
 
   describe('when using a custom directory with a custom extension', function () {
 
-    it('setting locales should return appropiate locale', function () {
-      var my_setLocale = i18n.setLocale('de'),
-          my_getLocale = i18n.getLocale();
-      should.equal('de', my_setLocale);
-      should.equal('de', my_getLocale);
+    it('setting locales in scope should return appropiate locale', function () {
+      should.equal('de', i18n.setLocale(testScope, 'de'));
     });
 
-    it('and some basic translations should still work', function () {
-      i18n.setLocale('en');
-      should.equal(__('Hello'), 'Hello');
-      should.equal(__('Hello %s, how are you today?', 'Marcus'), 'Hello Marcus, how are you today?');
-      should.equal(__('Hello %s, how are you today? How was your %s.', 'Marcus', __('weekend')), 'Hello Marcus, how are you today? How was your weekend.');
+    it('getting locales in scope should return appropiate locale', function () {
+      i18n.setLocale(testScope, 'de')
+      should.equal('de', testScope.getLocale(testScope));
+    });
+
+    it('getting defaultLocale should return appropiate locale', function () {
+      should.equal('en', i18n.getLocale());
+    });
+
+    it('basic translations should work', function () {
+      i18n.setLocale(testScope, 'en');
+      should.equal(testScope.__('Hello'), 'Hello');
+      should.equal(testScope.__('Hello %s, how are you today?', 'Marcus'), 'Hello Marcus, how are you today?');
+      should.equal(testScope.__('Hello %s, how are you today? How was your %s.', 'Marcus', testScope.__('weekend')), 'Hello Marcus, how are you today? How was your weekend.');
+    });
+
+    it('the directory should exist', function () {
+      var stats = fs.lstatSync('./testlocales');
+      should.exist(stats);
+    });
+
+    it('the files should exist', function () {
+      var statsde = fs.lstatSync('./testlocales/de.json'),
+          statsen = fs.lstatSync('./testlocales/en.json');
+      should.exist(statsde);
+      should.exist(statsen);
     });
 
   });
