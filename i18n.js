9b53fba0ac76bb960b53575dbca5420f7ad31796
Marcus Spiegel <spiegel@uscreen.de>
2013-07-06 17:31:03 +0200

    version bump

--- i18n.js
+++ i18n.js
@@ -3,7 +3,7 @@
  * @link        https://github.com/mashpie/i18n-node
  * @license     http://opensource.org/licenses/MIT
  *
- * @version     0.4.0
+ * @version     0.4.1
  */
 
 // dependencies and "private" vars
@@ -22,7 +22,7 @@ var vsprintf = require('sprintf').vsprintf,
 // public exports
 var i18n = exports;
 
-i18n.version = '0.4.0';
+i18n.version = '0.4.1';
 
 i18n.configure = function i18nConfigure(opt) {
 
