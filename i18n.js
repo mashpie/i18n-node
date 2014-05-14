96d87f505d26bc1ac5b5ed91fdec630251b0131d
Andrew <andrew.sygyda@gmail.com>
2014-05-14 03:19:35 +0300

    Update i18n.js

--- i18n.js
+++ i18n.js
@@ -69,8 +69,6 @@ i18n.init = function i18nInit(request, response, next) {
       if (!response.locale) response.locale = request.locale;
 
       if (response.locals) {
-//        applyAPItoObject(request, response.locals); ->this applied api to response.locals
-//        if it exist (var object = response || request;)
           applyAPItoObject(request);
 
         // register locale to res.locals so hbs helpers know this.locale
