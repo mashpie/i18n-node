9c8c1e9777b53387ba4cc047fe94eeffede1d418
j <jacobparker1992@gmail.com>
2014-12-23 20:37:12 +0000

    Support for argument middleware, joining arrays

---
+++ test/i18n.middleware.js
@@ -0,0 +1,26 @@
+/*jslint nomen: true, undef: true, sloppy: true, white: true, stupid: true, passfail: false, node: true, plusplus: true, indent: 2 */
+
+// now with coverage suport
+var should = require("should");
+
+
+
+describe('Basic Middleware', function () {
+  it('should convert lists', function () {
+    var i18n = process.env.EXPRESS_COV ? require('../i18n-cov') : require('../i18n');
+
+    i18n.configure({
+      locales: ['en', 'de'],
+      directory: './locales',
+      register: global,
+      middleware: [i18n.joinArrays]
+    });
+
+    i18n.setLocale('en');
+    should.equal(__('%s', ['one']), 'one');
+    should.equal(__('%s', ['one', 'two']), 'one and two');
+    should.equal(__('%s', ['one', 'two', 'three']), 'one, two, and three');
+
+    should.equal(__('You must fill in a value for your %s', ['email', 'password']), 'You must fill in a value for your email and password');
+  });
+});
