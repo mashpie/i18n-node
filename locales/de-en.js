4bb92bc7372f6df4ca4e57773255dd92e1919461
Adrian Wilczek <adrian.wilczek@rspective.pl>
2015-07-09 12:20:30 +0200

    Base files for locales - tests simplification

--- locales/de-en.js
+++
@@ -1,3 +0,0 @@
-{
-	"Hello %s, how are you today? How was your %s.": "Hello %s, how are you today? How was your trip to Berlin?"
-}
--- test/i18n.base.js
+++ test/i18n.base.js
@@ -5,55 +5,42 @@ var I18n = require('../i18n'),
 module.exports = {
 	'check base': function () {
 		var i18n = new I18n({
-			locales: ['at-de', 'de-en'],
+			locales: ['at-de'],
 			base: function(locale) { return locale.slice(-2); }
 		});
 
 		i18n.setLocale('at-de');
 		assert.equal(i18n.__('Hello'), 'Hallo');
 		assert.equal(i18n.__('Hello %s, how are you today? How was your %s.', 'Marcus'), 'Hallo Marcus, wie geht es dir heute? Wie war Ihre Reise nach Wien?');
-
-		i18n.setLocale('de-en');
-		assert.equal(i18n.__('Hello'), 'Hello');
-		assert.equal(i18n.__('Hello %s, how are you today? How was your %s.', 'Marcus'), 'Hello Marcus, how are you today? How was your trip to Berlin?');
 	},
 
 	'check base - returning nothing reads locale safely': function () {
 		var i18n = new I18n({
-			locales: ['at-de', 'de-en'],
-			base: function(locale) {}
+			locales: ['at-de'],
+			base: function() {}
 		});
 
 		i18n.setLocale('at-de');
 		assert.equal(i18n.__('Hello %s, how are you today? How was your %s.', 'Marcus'), 'Hallo Marcus, wie geht es dir heute? Wie war Ihre Reise nach Wien?');
-
-		i18n.setLocale('de-en');
-		assert.equal(i18n.__('Hello %s, how are you today? How was your %s.', 'Marcus'), 'Hello Marcus, how are you today? How was your trip to Berlin?');
 	},
 
 	'check base - returning not a string reads locale safely': function () {
 		var i18n = new I18n({
-			locales: ['at-de', 'de-en'],
-			base: function(locale) { return {}; }
+			locales: ['at-de'],
+			base: function() { return {}; }
 		});
 
 		i18n.setLocale('at-de');
 		assert.equal(i18n.__('Hello %s, how are you today? How was your %s.', 'Marcus'), 'Hallo Marcus, wie geht es dir heute? Wie war Ihre Reise nach Wien?');
-
-		i18n.setLocale('de-en');
-		assert.equal(i18n.__('Hello %s, how are you today? How was your %s.', 'Marcus'), 'Hello Marcus, how are you today? How was your trip to Berlin?');
 	},
 
 	'check base - throwing exception reads locale safely': function () {
 		var i18n = new I18n({
-			locales: ['at-de', 'de-en'],
-			base: function(locale) { throw new Error(); }
+			locales: ['at-de'],
+			base: function() { throw new Error(); }
 		});
 
 		i18n.setLocale('at-de');
 		assert.equal(i18n.__('Hello %s, how are you today? How was your %s.', 'Marcus'), 'Hallo Marcus, wie geht es dir heute? Wie war Ihre Reise nach Wien?');
-
-		i18n.setLocale('de-en');
-		assert.equal(i18n.__('Hello %s, how are you today? How was your %s.', 'Marcus'), 'Hello Marcus, how are you today? How was your trip to Berlin?');
 	}
 };
