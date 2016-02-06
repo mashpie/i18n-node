300917bf03ada0956e40017a6e439369f8f890f6
Marcus Spiegel <spiegel@uscreen.de>
2016-02-06 21:26:26 +0100

    added integration test on autoreload example

--- examples/node-http-autoreload/test.js
+++ examples/node-http-autoreload/test.js
@@ -1,25 +1,61 @@
-require('./node-http');
+require('./index');
 
 var Browser = require('zombie'),
-    visitLinks = require('../testlib/visitlinks'),
-    DE = new Browser({
-      headers: {
-        'accept-language': 'de'
-      }
-    }),
-    EN = new Browser(
-      {
-      headers: {
-        'accept-language': 'en'
-      }
-    });
+  fs = require('fs'),
+  should = require('should'),
+  DE = new Browser({
+    headers: {
+      'accept-language': 'de'
+    }
+  }),
+  EN = new Browser({
+    headers: {
+      'accept-language': 'en'
+    }
+  });
 
-describe('Using res.__() in a plain node.js setup http server to handle concurrent request correctly', function(){
-    describe('serial requests', function () {
-      visitLinks('series', 'test', EN, 'Hello', DE, 'Hallo');
-    });
+var file = __dirname + '/locales/en.json';
+var content = JSON.parse(fs.readFileSync(file));
 
-    describe('parallel requests', function () {
-      visitLinks('parallel', 'test', EN, 'Hello', DE, 'Hallo');
+describe('autoreload', function() {
+  it('should give current translations in EN', function(done) {
+    EN.visit('http://localhost:3000/test/', function() {
+      should.equal(EN.text('body'), 'Hello');
+      done();
     });
+  });
+
+  it('should give current translations in DE', function(done) {
+    DE.visit('http://localhost:3000/test/', function() {
+      should.equal(DE.text('body'), 'Hallo');
+      done();
+    });
+  });
+
+  it('should trigger reload of translations in EN', function(done) {
+    content['Hello'] = 'Hi';
+    fs.writeFileSync(file, JSON.stringify(content, null, "\t"), "utf8");
+    setTimeout(done, 100);
+  });
+
+  it('should give updated translations in EN', function(done) {
+    EN.visit('http://localhost:3000/test/', function() {
+      should.equal(EN.text('body'), 'Hi');
+      done();
+    });
+  });
+
+  it('should still give old translations in DE', function(done) {
+    DE.visit('http://localhost:3000/test/', function() {
+      should.equal(DE.text('body'), 'Hallo');
+      done();
+    });
+  });
+
+  it('should reset en.json', function(done) {
+    content['Hello'] = 'Hello';
+    fs.writeFileSync(file, JSON.stringify(content, null, "\t"), "utf8");
+    done();
+  });
 });
+
