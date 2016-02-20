0f1066c98ba7bbfa864f2d12b9668d1175b3c4e1
Dan Green <danon0404@gmail.com>
2016-02-20 15:25:21 +0600

    tests are updated

--- test/i18n.objectnotation.js
+++ test/i18n.objectnotation.js
@@ -41,6 +41,12 @@ describe('Object Notation', function () {
       should.throws(__('greeting.placeholder.loud', 'Marcus'));
     });
 
+    it('should return en translations as expected, then dot is first or last character', function () {
+      i18n.setLocale('en');
+      should.equal(__('. is first character'), 'Dot is first character');
+      should.equal(__('last character is .'), 'last character is Dot');
+    });
+
     it('should provide proper pluralization support, using object traversal notation', function () {
       i18n.setLocale('en');
       var singular = __n({singular: "cat", plural: "cat", locale: "de"}, 1),
