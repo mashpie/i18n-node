890dc4c29ba9873e043c29b34beccdea4ee4569b
Travis Nelson <travis.o.nelson@gmail.com>
2016-11-03 22:40:49 -0600

    Guarding for-in from unexpected items in loop
    Per http://eslint.org/docs/rules/guard-for-in
    
    Looping over objects with a for in loop will include properties that
    are inherited through the prototype chain. This behavior can lead to
    unexpected items in your for loop.

--- i18n.js
+++ i18n.js
@@ -912,7 +912,9 @@ module.exports = (function() {
     // this will implicitly write/sync missing keys
     // to the rest of locales
     for (var l in locales) {
-      translate(l, singular, plural, true);
+      if ({}.hasOwnProperty.call(locales, l)) {
+        translate(l, singular, plural, true);
+      }         
     }
   };
 
