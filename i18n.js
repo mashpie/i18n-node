f06678c0a4582dba314eecbc8b5ea6f167cf1fae
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-03-18 10:49:56 +0100

    feat(i18n.js) deprecated GLOBAL usage removed for global

--- i18n.js
+++ i18n.js
@@ -486,7 +486,7 @@ module.exports = (function() {
       locale === undefined &&
       typeof this.locale === 'string'
     ) {
-      if (register && register.GLOBAL) {
+      if (register && register.global) {
         targetLocale = '';
       } else {
         targetLocale = this.locale;
