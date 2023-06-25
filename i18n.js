9d6165cbd14805a6753daeb9f0dea4d1053bb038
Long Doan <hoanglong7421@gmail.com>
2023-06-25 18:19:59 +0200

    Use translation from default locale if provided locale is "undefined" or "null" for method i18n.__mf. Issue #501

--- i18n.js
+++ i18n.js
@@ -308,11 +308,10 @@ const i18n = function I18n(_OPTS = false) {
     // called like __({phrase: "Hello", locale: "en"})
     if (typeof phrase === 'object') {
       if (
-        typeof phrase.locale === 'string' &&
         typeof phrase.phrase === 'string'
       ) {
         msg = phrase.phrase
-        targetLocale = phrase.locale
+        targetLocale = getLocaleFromObject(phrase)
       }
     }
     // called like __("Hello")
